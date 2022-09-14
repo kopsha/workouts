0fe13ea704693ff96bb05aa9086865c7986581ee
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-09-14 09:23:58 +0300

    easy peasy

--- ten-minutes-walk.py
+++ ten-minutes-walk.py
@@ -1,17 +1,32 @@
 def is_valid_walk(walk):
-    steps = dict()
-    pass
+
+    if len(walk) != 10:
+        return False
+
+    x, y = 0, 0
+    deltas = dict(
+        e=(-1, 0),
+        w=(+1, 0),
+        n=(0, -1),
+        s=(0, +1),
+    )
+
+    for step in walk:
+        dx, dy = deltas[step]
+        x, y = x + dx, y + dy
+
+    return (x, y) == (0, 0)
 
 
 def test_walk():
     test_values = (
-        (["w", "False"]),
+        (["w"], False),
         (["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"], True),
         (["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"], False),
         (["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"], False),
     )
 
-    for path, expected in test_values.items():
+    for path, expected in test_values:
         actual = is_valid_walk(path)
         ppath = "".join(path)
         assert (
