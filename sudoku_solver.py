597115c30ff7b40a561d010c5cf272727bb30939
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-09-16 18:28:15 +0300

    non-recursive is super slow

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,6 @@
-from itertools import permutations, chain
+from itertools import chain
+from copy import deepcopy
+from collections import deque
 
 
 SPACE = list(range(10))
@@ -20,36 +22,21 @@ def is_solved(puzzle):
 
 
 def sudo_print(puzzle, title=""):
+
+    if not puzzle:
+        return
+
     print(f"    {title}")
+
+    row_fmt = "|" + "|".join(["%s" * 3] * 3) + "|"
+    row_sep = "+" + "+".join(["-" * 3] * 3) + "+"
+
     for i, row in enumerate(puzzle):
         if i % 3 == 0:
-            print("+---" * 3 + "+")
-        augmented = [
-            "|" * int(j % 3 == 0) + (str(x) if x else " ") for j, x in enumerate(row)
-        ] + [
-            "|",
-        ]
-        print("".join(augmented))
+            print(row_sep)
+        print(row_fmt % tuple(str(x) if x else " " for x in row))
     print("+---" * 3 + "+")
     print("    ", "solved" * is_solved(puzzle))
-
-
-def sudoku_permutations(cell):
-    """Generates all permutations given a starting position"""
-
-    variable = set(SPACE) - set(cell)
-    fixed = dict()
-    var_pos = dict()
-
-    for i, value in enumerate(cell):
-        if value:
-            fixed[i] = value
-        else:
-            var_pos[i] = len(var_pos)
-
-    for x in permutations(variable):
-        next_pos = [fixed[i] if i in fixed else x[var_pos[i]] for i in range(len(cell))]
-        yield next_pos
 
 
 def valid_moves_of(pos, puzzle):
@@ -103,12 +90,59 @@ def sudoku(puzzle):
 
     return None
 
+
+def canonical(puzzle):
+    return "".join(str(x) if x else " " for x in chain.from_iterable(puzzle))
+
+
+def sudo_show(flatline):
+    n = 3
+    "Display grid from a string (values in row major order with blanks for unknowns)"
+    fmt = "|".join(["%s" * n] * n)
+    sep = "+".join(["-" * n] * n)
+    for i in range(n):
+        for j in range(n):
+            offset = (i * n + j) * n ** 2
+            print(fmt % tuple(flatline[offset : offset + n ** 2]))
+        if i != n - 1:
+            print(sep)
+
+
+def generate_sudoku_positions(puzzle):
+    possible_moves = list()
+    for i, row in enumerate(puzzle):
+        for j, value in enumerate(row):
+            pos = i, j
+            if value == 0:
+                options = valid_moves_of(pos, puzzle)
+                if options:
+                    possible_moves.append((pos, options))
+
+    possible_moves = sorted(possible_moves, key=lambda pair: len(pair[1]))[:3]
+
+    for (row, col), options in possible_moves:
+        for opt in options:
+            puzzle[row][col] = opt
+            yield deepcopy(puzzle)
+            puzzle[row][col] = 0
+
+
 def solve(puzzle):
     """hopefully a faster version"""
+    sudo_print(puzzle, "given")
+    queue = deque([puzzle])
+    cnt = 0
+    while not is_solved(puzzle):
+        cnt += 1
+        if cnt % 100000 == 0:
+            sudo_print(puzzle, f"{cnt} {len(queue)=}")
 
+        for new_puzzle in generate_sudoku_positions(puzzle):
+            queue.appendleft(new_puzzle)
+        puzzle = queue.pop()
 
-
-    return sudoku(puzzle)
+    sudo_print(puzzle, "solution")
+    return puzzle
 
 
 def _test_debug():
@@ -132,7 +166,7 @@ def _test_debug():
     assert False
 
 
-def test_basic_solver():
+def test_recursive_solver():
     puzzle = [
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -160,6 +194,36 @@ def test_basic_solver():
     actual = sudoku(puzzle)
     assert actual == expected
 
+
+def test_basic_solver():
+    puzzle = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+
+    expected = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9],
+    ]
+
+    actual = solve(puzzle)
+    assert actual == expected
+
+
 def test_optimized_solver():
     puzzle = [
         [9, 0, 0, 0, 8, 0, 0, 0, 1],
@@ -186,5 +250,4 @@ def test_optimized_solver():
     ]
 
     actual = solve(puzzle)
-    sudo_print(actual)
     assert actual == expected
