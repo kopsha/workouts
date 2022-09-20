bc68b6474224abd55855a3fbcb84a404fcf563f2
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-09-20 20:39:22 +0300

    pretty

--- functional_parsing.py
+++ functional_parsing.py
@@ -1,17 +1,33 @@
-
 class RegExp:
     def __init__(self, *args):
         self.args = args
+
     def __repr__(self):
         args = ", ".join(map(repr, self.args))
         return f"{self.__class__.__name__}({args})"
+
     def __eq__(self, other):
         return type(self) is type(other) and self.args == other.args
-class Any(RegExp): pass
-class Normal(RegExp): pass
-class Or(RegExp): pass
-class Str(RegExp): pass
-class ZeroOrMore(RegExp): pass
+
+
+class Any(RegExp):
+    pass
+
+
+class Normal(RegExp):
+    pass
+
+
+class Or(RegExp):
+    pass
+
+
+class Str(RegExp):
+    pass
+
+
+class ZeroOrMore(RegExp):
+    pass
 
 
 def parse_regexp(indata):
@@ -19,10 +35,11 @@ def parse_regexp(indata):
     return Any()
 
 
-
 def check(indata, expected):
     actual = parse_regexp(indata)
-    assert actual == expected, f"Given {indata!r} expecting {expected!r} but got {actual!r} instead."
+    assert (
+        actual == expected
+    ), f"Given {indata!r} expecting {expected!r} but got {actual!r} instead."
 
 
 def test_basic_expressions():
@@ -47,11 +64,14 @@ def test_precedence():
     check("(a|b)*", ZeroOrMore(Or(Normal("a"), Normal("b"))))
 
 
-def test_other_examples()
+def test_other_examples():
     check("a", Normal("a"))
     check("ab", Str([Normal("a"), Normal("b")]))
     check("a.*", Str([Normal("a"), ZeroOrMore(Any())]))
-    check("(a.*)|(bb)", Or(Str([Normal("a"), ZeroOrMore(Any())]), Str([Normal("b"), Normal("b")])))
+    check(
+        "(a.*)|(bb)",
+        Or(Str([Normal("a"), ZeroOrMore(Any())]), Str([Normal("b"), Normal("b")])),
+    )
 
 
 def test_invalid_examples():
@@ -64,5 +84,19 @@ def test_invalid_examples():
 
 
 def test_complex_examples():
-    check("((aa)|ab)*|a", Or(ZeroOrMore(Or(Str([Normal("a"), Normal("a")]), Str([Normal("a"), Normal("b")]))), Normal("a")))
-    check("((a.)|.b)*|a", Or(ZeroOrMore(Or(Str([Normal("a"), Any()]), Str([Any(), Normal("b")]))), Normal("a")))
+    check(
+        "((aa)|ab)*|a",
+        Or(
+            ZeroOrMore(
+                Or(Str([Normal("a"), Normal("a")]), Str([Normal("a"), Normal("b")]))
+            ),
+            Normal("a"),
+        ),
+    )
+    check(
+        "((a.)|.b)*|a",
+        Or(
+            ZeroOrMore(Or(Str([Normal("a"), Any()]), Str([Any(), Normal("b")]))),
+            Normal("a"),
+        ),
+    )
