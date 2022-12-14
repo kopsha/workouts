6cff160f6003ea44f4c677ea80664b480d8c7c30
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-12-14 21:16:25 +0200

    found a more accurate movement computation

--- double_pod_racing.py
+++ double_pod_racing.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
-import math
-import cmath
-import sys
+from math import degrees, radians, remainder
+from cmath import rect, polar, phase, pi
 from collections import namedtuple, deque
 from typing import Iterable
+import sys
 
 
 CP_RADIUS = 600
@@ -13,6 +13,9 @@ TURN_SPEED = 400
 Coord = namedtuple("Coord", ["x", "y"])
 Pod = namedtuple("Pod", ["x", "y", "vx", "vy", "angle", "cpid"])
 
+
+def clamp(value: float, left: float, right: float):
+    return max(left, min(value, right))
 
 class PodRacer:
     def __init__(self, name, pod: Pod, checkpoints: list[Coord]) -> None:
@@ -28,7 +31,9 @@ class PodRacer:
 
         self.velocity = complex(pod.vx, pod.vy)
         self.speed_trace.append(int(round(abs(self.velocity))))
-        self.v_angle = cmath.phase(self.velocity)
+        self.v_angle = phase(self.velocity)
+        self.angle = radians(pod.angle)
+        self.facing = pod.angle
 
         self.cpid = pod.cpid
         self.cp = complex(*checkpoints[pod.cpid])
@@ -39,54 +44,70 @@ class PodRacer:
     def touch(self, target: complex) -> complex:
         """Will barely touch the checkpoint"""
         along = self.position - target
-        touch_delta = cmath.rect(CP_GRAVITY - CP_RADIUS, cmath.phase(along))
+        touch_delta = rect(CP_GRAVITY - CP_RADIUS, phase(along))
         return target - touch_delta
 
     def drift_towards(self, target) -> tuple[complex, int]:
         thrust = 100
         distance = abs(self.position - self.cp)
 
-        target_dev = math.remainder(
-            cmath.phase(target - self.position) - self.v_angle, cmath.pi
+        target_dev = remainder(
+            phase(target - self.position) - self.v_angle, pi
         )
-        next_cp_dev = math.remainder(
-            self.v_angle - cmath.phase(self.next_cp - self.cp), cmath.pi
+        next_cp_dev = remainder(
+            self.v_angle - phase(self.next_cp - self.cp), pi
         )
 
         if distance <= sum(self.speed_trace):
             if self.speed_trace[-1] > 300:
                 # change target sooner to allow rotation
                 target = self.next_cp
-            if abs(next_cp_dev) > cmath.pi / 2:
+            if abs(next_cp_dev) > pi / 2:
                 thrust = 0
         else:
-            if target_dev > 3 * cmath.pi / 4:
+            if target_dev > 3 * pi / 4:
                 thrust = 0 if self.speed_trace[-1] > 250 else 33
-            elif target_dev > cmath.pi / 2:
+            elif target_dev > pi / 2:
                 thrust = 66
 
         return target, thrust
 
     def correct_rotation(self) -> complex:
-        target_dev = math.remainder(
-            cmath.phase(self.target - self.position) - self.v_angle, cmath.pi
+        target_dev = remainder(
+            phase(self.target - self.position) - self.v_angle, pi
         )
         desired = self.target - self.position
-        rotate = cmath.rect(1, target_dev / 4)
+        rotate = rect(1, target_dev / 4)
         return desired * rotate + self.position
 
     @property
     def next_position(self):
-        return self.position + self.velocity
+        """a more accurate next position computation"""
+        if self.thurst == "SHIELD":
+            thrust = 0
+        elif self.thurst == "BOOST":
+            thrust = 650
+        else:
+            thrust = self.thurst
+
+        t_angle = phase(self.target - self.position)
+        dev = t_angle - self.angle
+        rot = clamp(dev, -pi/10, pi/10)
+
+        acc = rect(thrust, self.angle + rot)
+        movement = self.velocity + acc
+        new_position = self.position + movement
+
+        return new_position
 
     def defend_on_collision(self, opponents: Iterable[PodRacer]):
         for opp in opponents:
             next_dist = int(round(abs(self.next_position - opp.next_position)))
             speed_diff = abs(self.speed_trace[-1] - opp.speed_trace[-1])
-            collision_angle = self.v_angle - opp.v_angle
+            collision_angle = remainder(self.v_angle - opp.v_angle, pi)
 
             if next_dist <= 900 and (
-                abs(collision_angle) > cmath.pi / 4 or speed_diff > 250
+                abs(collision_angle) > pi / 4 or speed_diff > 250
             ):
                 print(
                     f"--- {self.name} vs {opp.name} => {next_dist} ---", file=sys.stderr
@@ -96,25 +117,36 @@ class PodRacer:
                     file=sys.stderr,
                 )
                 print(
-                    f"Collision angle: {int(math.degrees(collision_angle))}",
+                    f"Collision angle: {int(degrees(collision_angle))}",
                     file=sys.stderr,
                 )
                 self.thurst = "SHIELD"
                 return
-            elif next_dist <= 1800 and collision_angle <= cmath.pi / 5:
+            elif next_dist <= 1800 and collision_angle <= pi / 5:
                 # move in-front of opponent
                 x = (self.target.real * 2 + opp.position.real) / 3
                 y = (self.target.imag * 2 + opp.position.imag) / 3
                 self.target = complex(x, y)
                 return
 
+    def can_reach_checkpoint(self):
+        """a more accurate next position computation"""
+
+        last_distance = abs(self.last_position - self.cp)
+        distance = abs(self.position - self.cp)
+        next_distance = abs(self.next_position - self.cp)
+
+        print(f"closing? {last_distance - distance} > {distance - next_distance}", file=sys.stderr)
+
+        return True
+
     def can_boost(self) -> bool:
         if self.has_boost:
-            target_dev = math.remainder(
-                cmath.phase(self.target - self.position) - self.v_angle, cmath.pi
+            target_dev = remainder(
+                phase(self.target - self.position) - self.v_angle, pi
             )
             distance = abs(self.position - self.cp)
-            if abs(target_dev) < cmath.pi / 36 and distance > 4500:
+            if abs(target_dev) < pi / 36 and distance > 4500:
                 return True
         return False
 
@@ -132,8 +164,9 @@ class PodRacer:
 
     def __repr__(self):
         return (
-            f"D:{int(round(abs(self.cp - self.position))):4} m "
+            f"F<:{self.facing}° "
             f"V:{int(round(abs(self.velocity))):4} m/s O:{self.thurst:4} "
+            f"D:{int(round(abs(self.cp - self.position))):4} m "
             f"has boost: {self.has_boost}"
         )
 
@@ -199,6 +232,8 @@ def main():
     him1 = PodRacer("him1", pods["him_first"], layout["checkpoints"])
     him2 = PodRacer("him2", pods["him_second"], layout["checkpoints"])
 
+    print(repr(me1), file=sys.stderr)
+    print(repr(me2), file=sys.stderr)
     print(me1)
     print(me2)
 
@@ -211,6 +246,9 @@ def main():
         him1.update(pods["him_first"], layout["checkpoints"])
         him2.update(pods["him_second"], layout["checkpoints"])
 
+        # TODO:
+        # - evaluate, can we reach the target ?
+        # - avoid team collisions too
         if hold_boost > 0:
             hold_boost -= 1
         else:
@@ -223,6 +261,9 @@ def main():
 
         me1.defend_on_collision((him1, him2))
         me2.defend_on_collision((him2, him1))
+
+        me1.can_reach_checkpoint()
+        me2.can_reach_checkpoint()
 
         print(repr(me1), file=sys.stderr)
         print(repr(me2), file=sys.stderr)
