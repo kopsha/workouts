07327ec772a532ba59337920323d6e8f4d15c6b7
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2022-12-18 01:58:36 +0200

    bezier paths look good

--- bezier_pod_racing.py
+++ bezier_pod_racing.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from typing import Iterable
 from math import degrees, radians, remainder
 from cmath import rect, polar, phase, pi
 from collections import namedtuple, deque
-from typing import Iterable
+from itertools import zip_longest
 import sys
 import pygame
 import numpy as np
@@ -85,9 +86,9 @@ class PodRacer:
             elif abs(next_cp_dev) > pi / 4:
                 thrust = 50
         else:
-            if target_dev > 3 * pi / 4:
+            if abs(target_dev) > 3 * pi / 4:
                 thrust = 0 if self.speed_trace[-1] > 250 else 33
-            elif target_dev > pi / 2:
+            elif abs(target_dev) > pi / 2:
                 thrust = 66
 
         return target, thrust
@@ -251,12 +252,25 @@ def lerp(a: Coord, b: Coord, t: float) -> Coord:
     return Coord(rx, ry)
 
 
+def quadratic(a: float, b: float, c: float, t: float) -> float:
+    rc = (1 - t) ** 2 * a + 2 * (1 - t) * t * b + t**2 * c
+    return rc
+
+
+def quadratic_bezier(a: Coord, b: Coord, c: Coord) -> list[Coord]:
+    curve = [
+        Coord(quadratic(a.x, b.x, c.x, t), quadratic(a.y, b.y, c.y, t))
+        for t in np.linspace(0, 1, 34)
+    ]
+    return curve
+
+
 def cubic(a: float, b: float, c: float, d: float, t: float) -> float:
     rc = (
         (1 - t) ** 3 * a
         + 3 * (1 - t) ** 2 * t * b
-        + 3 * (1 - t) * t ** 2 * c
-        + t ** 3 * d
+        + 3 * (1 - t) * t**2 * c
+        + t**3 * d
     )
     return rc
 
@@ -343,17 +357,69 @@ class LayoutPainter(PicassoEngine):
             pygame.event.post(bye)
 
     def post_init(self):
-        mono = pygame.font.SysFont("monospace", 168)
-        points = SAMPLE_LAYOUT["checkpoints"]
+        self.font = pygame.font.SysFont("monospace", 168)
+
+        layout = SAMPLE_LAYOUT["checkpoints"]
+        self.mark_points(layout)
+
+        last_mirror_cp = layout[0]
+        cps = list()
+        fcps = list()
+
+        for left, right, tow in zip(layout, layout[1:] + layout[:1], layout[2:] + layout[:2]):
+          
+            print(layout.index(left), "->", layout.index(right), "->", layout.index(tow))
+
+            zcp, mirror_zcp = find_control_points(left, right, tow)
+            cp, mirror_cp = Coord(*to_coords(zcp)), Coord(*to_coords(mirror_zcp))
+
+            fcps.append(cp)
+            cps.append(last_mirror_cp)
+
+            curve = cubic_bezier(left, last_mirror_cp, cp, right)
+            pygame.draw.lines(
+                self.canvas, GRAY, closed=False, points=curve, width=13
+            )
+            last_mirror_cp = mirror_cp
+        
+        self.mark_control_points(fcps)
+        self.mark_control_points(cps, prefix="__")
+
+    def mark_points(self, points: list[Coord]) -> None:
         for i, point in enumerate(points):
-            print(point)
             pygame.draw.circle(self.canvas, WHITE, point, radius=100)
-            labelSurface = mono.render(str(i + 1), True, GRAY)
+            labelSurface = self.font.render(str(i + 1), True, GRAY)
             aside = Coord(point.x + 89, point.y + 55)
             self.canvas.blit(labelSurface, aside)
 
-        curve = cubic_bezier(*points[0:4])
-        pygame.draw.lines(self.canvas, GRAY, closed=False, points=curve, width=13)
+    def mark_control_points(self, points: list[Coord], prefix="") -> None:
+        for i, point in enumerate(points):
+            pygame.draw.circle(self.canvas, WHITE, point, radius=34)
+            labelSurface = self.font.render(f"{prefix}z{i}", True, GRAY)
+            aside = Coord(point.x + 89, point.y - 144)
+            self.canvas.blit(labelSurface, aside)
+
+
+def find_control_points(pos: Coord, left: Coord, right: Coord):
+    position = complex(*pos)
+    target = complex(*left)
+    towards = complex(*right)
+
+    target_angle = phase(target - position)
+    towards_angle = phase(towards - target)
+    rev_half = pi - (target_angle - towards_angle)/2
+
+    print(int(degrees(target_angle)), int(degrees(towards_angle)), int(degrees(rev_half)))
+
+    mid_target = (target - position) / 4
+    if rev_half > pi:
+        rel_opp = mid_target * rect(1, rev_half)
+        rel = mid_target * rect(1, rev_half + pi)
+    else:
+        rel = mid_target * rect(1, rev_half)
+        rel_opp = mid_target * rect(1, rev_half + pi)
+    return target - rel, target - rel_opp
+
 
 def bezier_main():
     with LayoutPainter() as engine:
