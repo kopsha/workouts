96e49587354c0b34eff8e20b8a87c7b2e754cf26
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2023-03-11 15:49:37 +0200

    zipping rocks

--- pressed-config.py
+++ pressed-config.py
@@ -1,13 +1,100 @@
 #!/usr/bin/env python3
 import yaml
+import mimetypes
+import gzip
+
+from faker import Faker
+from time import perf_counter_ns
 
 
-def main(filename):
+def fake_channels(count=2500):
+    faker = Faker()
+    channels = list()
+    for i in range(count):
+        fake_channel = {
+            "id": faker.uuid4(),
+            "name": f"#{i} {faker.bs().title()}",
+            "oboChannelId": f"oboId{faker.pyint()}",
+            "drmContentId": faker.uuid4(),
+            "cleanupThreshold": faker.license_plate(),
+            "intendedTrackTypeId": faker.uuid4(),
+            "interimManifestDuration": faker.ssn(),
+            "interimManifestTime": [0, 15, 30, 45],
+            "manifestUrls": [
+                f"http://{faker.ipv4()}{faker.file_path(depth=4)}",
+                f"http://{faker.ipv4()}{faker.file_path(depth=4)}",
+            ],
+            "packagerId": f"pack{faker.pyint(max_value=9)}",
+            "sourceId": f"sourceId{faker.pyint(max_value=9)}",
+            "swmLength": "PT2H",
+            "tenantIds": [faker.country_code().lower()],
+            "updatePeriod": "PT7S",
+        }
+        channels.append(fake_channel)
+    return channels
+
+def blow_up_channels(filename):
     with open(filename, "rt") as file:
         config = yaml.safe_load(file)
+
+    config["channels"] = fake_channels(2500)
+
+    with open(filename, "wt") as outfile:
+        yaml.safe_dump(config, outfile)
     
-    print(config)
+    print("Done, faked", len(config["channels"]), "channels.")
 
 
+def compress(filename):
+    mimetypes.init()
+
+    with open(filename, "rt") as file:
+        content = file.read()
+
+    clear_buffer = content.encode("utf-8")
+
+    ref_ns = perf_counter_ns()
+    pressed_buffer = gzip.compress(clear_buffer)
+    end_ns = perf_counter_ns()
+
+    with open(filename+".gz", "wb") as zipped:
+        zipped.write(pressed_buffer)
+
+    print(f"Read  {len(clear_buffer):12,} bytes")
+    print(f"Wrote {len(pressed_buffer):12,} bytes")
+    ratio = len(pressed_buffer) / len(clear_buffer)
+    print(f"Ratio: {100 * ratio:.2f}%")
+    print(f"Compression took {(end_ns - ref_ns) / 1_000_000:.3f} ms")
+
+
+def smart_load(filename):
+    _, menc = mimetypes.guess_type(filename)
+
+    if menc == "gzip":
+        print("it's zipped")
+
+        with open(filename, "rb") as zipfile:
+            content = zipfile.read()
+            
+        ref_ns = perf_counter_ns()
+        buffer = gzip.decompress(content)
+        end_ns = perf_counter_ns()
+        text = buffer.decode("utf-8")
+
+        print(f"Decompression took {(end_ns - ref_ns) / 1_000_000:.3f} ms")
+
+        with open(filename+".check", "wt") as outfile:
+            outfile.write(text)
+
+    else:
+        with open(filename, "rt") as textfile:
+            text = textfile.read()
+
+    config = yaml.safe_load(text)
+    print("loaded config has", len(config["channels"]), "channels")
+
+    
 if __name__ == "__main__":
-    main("dummy-configuration.yaml")
+    blow_up_channels("dummy-configuration.yaml")
+    compress("dummy-configuration.yaml")
+    smart_load("dummy-configuration.yaml.gz")
