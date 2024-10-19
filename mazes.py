2f7a7fb133d09504400aa940e7e918af5c05de6e
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2024-10-19 20:57:22 +0300

    There are still problems with shortcutting nodes

--- mazes.py
+++ mazes.py
@@ -4,13 +4,13 @@ from enum import IntEnum
 from functools import partial
 from heapq import heappop, heappush
 from math import inf
-from operator import sub
+from operator import itemgetter, sub
 from typing import Optional
 
 rows, cols, alarm_count = [int(i) for i in input().split()]
 ROWS = rows + 2
 COLS = cols + 2
-CLEAR = {".", "C", "T", "o"}
+CLEAR = {".", "C", "T", "o", "x"}
 WALLS = {"#", "?"}
 DIRECTION = {
     (-1, 0): "UP",
@@ -149,9 +149,26 @@ Edge = namedtuple("Edge", ["vertex", "weight"])
 class UnidirectedWeightedGraph:
     """Every node is just an index"""
 
-    def __init__(self, nodes):
-        self.node_index = {node.pos: i for i, node in enumerate(nodes)}
+    def __init__(self, nodes, goal):
         self.adjacency = [list() for _ in range(len(nodes))]
+        self.node_index = {node.pos: i for i, node in enumerate(nodes)}
+
+        if isinstance(goal, list):
+            for gi in goal:
+                if gi not in self.node_index:
+                    nearby_goal = [
+                        (edi, n) for n in nodes if (edi := n.edge_distance(gi)) != inf
+                    ]
+                    nearby_goal.sort(key=itemgetter(0))
+                    _, chosen = nearby_goal[0]
+                    self.node_index[gi] = self.node_index[chosen.pos]
+        elif goal not in self.node_index:
+            nearby_goal = [
+                (edi, n) for n in nodes if (edi := n.edge_distance(goal)) != inf
+            ]
+            nearby_goal.sort(key=itemgetter(0))
+            _, chosen = nearby_goal[0]
+            self.node_index[goal] = self.node_index[chosen.pos]
 
         for i, node in enumerate(nodes):
             for link in node.links:
@@ -159,7 +176,7 @@ class UnidirectedWeightedGraph:
                 self.adjacency[i].append(Edge(self.node_index[link.pos], distance))
 
 
-def dijkstra(graph, start, dest):
+def dijkstra(graph, start, dest, nodes):
     size = len(graph.node_index)
     trail = [None] * size
     distance = [inf] * size
@@ -169,7 +186,9 @@ def dijkstra(graph, start, dest):
     pos = start
     while q:
         _, pos = heappop(q)
+        print(">", nodes[pos].pos, "->", graph.adjacency[pos], file=sys.stderr)
         if pos == dest:
+            print("found", pos, dest, file=sys.stderr)
             break
         dist = distance[pos]
         for edge in graph.adjacency[pos]:
@@ -179,17 +198,29 @@ def dijkstra(graph, start, dest):
                 trail[edge.vertex] = pos
                 heappush(q, (alt, edge.vertex))
 
-    path = [dest]
-    while (pos := trail[pos]) is not None:
-        path.append(pos)
+    if pos == dest:
+        path = [dest]
+        while (pos := trail[pos]) is not None:
+            path.append(pos)
+    else:
+        path = []
 
     return path
 
 
 def find_path(pos, goal, nodes, graph):
     start = graph.node_index[pos]
-    dest = graph.node_index[goal]
-    ipath = dijkstra(graph, start, dest)
+    if isinstance(goal, list):
+        ipath = []
+        for gi in goal:
+            dest = graph.node_index[gi]
+            print("trying from", pos, "to", gi, file=sys.stderr)
+            ipath = dijkstra(graph, start, dest, nodes)
+            if ipath:
+                break
+    else:
+        dest = graph.node_index[goal]
+        ipath = dijkstra(graph, start, dest, nodes)
 
     path = [nodes[i].pos for i in ipath]
     return path
@@ -243,19 +274,13 @@ while True:
             line = replace_index(line, col, "o")
         maze.append("#" + line + "#")
 
-    print(*maze, sep="\n", file=sys.stderr)
-
     nodes, foggy = parse(maze, pos)
 
     # looking for closest foggy node
     _dist = partial(square_dist, pos)
     foggy.sort(key=_dist)
     foggy_dist = [(square_dist(pos, fi), fi) for fi in foggy]
-    print("foggy", foggy, file=sys.stderr)
-    # print("nodes", nodes, file=sys.stderr)
-
-    graph = UnidirectedWeightedGraph(nodes)
-    # print(graph.adjacency, file=sys.stderr)
+    print("foggy", foggy[:5], file=sys.stderr)
 
     if status == State.EXPAND:
         if not foggy:
@@ -267,17 +292,36 @@ while True:
             status = State.TO_HOME
 
     if status == State.EXPAND:
-        goal = next(iter(foggy))
+        goal = foggy[:5]
     elif status == State.TO_CC:
         goal = cc
     else:
         goal = home
 
+    # maze[goal[0]] = replace_index(maze[goal[0]], goal[1], "x")
+    print(*maze, sep="\n", file=sys.stderr)
     print("ss:", status, goal, file=sys.stderr)
-    trail = find_path(pos, goal, nodes, graph)
-    print("trail:", trail, file=sys.stderr)
 
-    towards = trail[-2]  # last position should be the current one
-    diff = tuple(map(sign, map(sub, trail[-2], trail[-1])))
+    # print("nodes", nodes, file=sys.stderr)
+
+    graph = UnidirectedWeightedGraph(nodes, goal)
+    # print(graph.adjacency, file=sys.stderr)
+    trail = find_path(pos, goal, nodes, graph)
+
+    print("trail:", trail, file=sys.stderr)
+    if len(trail) > 1:
+        towards = trail[-2]  # last position should be the current one
+        diff = tuple(map(sign, map(sub, towards, trail[-1])))
+    elif len(trail) == 1:
+        towards = goal[0] if isinstance(goal, list) else goal
+        diff = tuple(map(sign, map(sub, towards, pos)))
+    else:
+        trail = find_path(pos, cc, nodes, graph)
+        if len(trail) > 1:
+            diff = tuple(map(sign, map(sub, trail[-2], trail[-1])))
+        elif len(trail) == 1:
+            diff = tuple(map(sign, map(sub, cc, pos)))
+        else:
+            raise RuntimeError
 
     print(DIRECTION[diff])
